7ff430bbf904093eab9aeac2a42ad0b2e83c6bdc
sak23042006 <138766433+sak23042006@users.noreply.github.com>
2023-09-23 10:02:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-conditional-statements
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-svjmru)
---
+++ script.js
@@ -0,0 +1,58 @@
+const submitButton1 = document.getElementById('button1');
+const commentContainer1 = document.getElementById('comment-container1');
+var message1;
+
+submitButton1.onclick = () => {
+  let numberInput1 = document.getElementById('number-input1').value;
+
+  // Challenge 1: Use if else condition to store the respective comments in the variable "message1" based on the score input.
+  // when the user gives a number input and clicks on the submit button, the respective comments should be displayed.
+  // Here are the values and their respective comments
+  // Score of less than 0 or more than 100 — “This is not possible. An error has occurred.”
+  // Score of 0 to 19 — “That was a terrible score!”
+  // Score of 20 to 39 — “You know some things. Needs improvement.”
+  // Score of 40 to 69 — “You did a passable job, not bad!”
+  // Score of 70 to 89 — “That’s a great score. You really know your stuff.”
+  // Score of 90 to 100 — “What an amazing score!”
+
+  if (numberInput1 <0 || numberInput1>100){
+    message1= "This is not possible. An error has occurred."
+  }
+  else if(numberInput1 >=0 && numberInput1<20){
+    message1= "That was a terrible score!"
+  }
+  else if(numberInput1 >=20 && numberInput1<40){
+    message1= "You know some things. Needs improvement."
+  }
+  else if(numberInput1 >=40 && numberInput1<70){
+    message1= "You did a passable job, not bad!"
+  }
+  else if(numberInput1 >=70 && numberInput1<90){
+    message1= "That’s a great score. You really know your stuff."
+  }
+  else{
+    message1= "What an amazing score!"
+  }
+
+  commentContainer1.innerHTML = message1;
+};
+
+const submitButton2 = document.getElementById('button2');
+const commentContainer2 = document.getElementById('comment-container2');
+var message2;
+
+submitButton2.onclick = () => {
+  let numberInput2 = document.getElementById('number-input2').value;
+
+  // Challenge 2: Use Switch statements to store the respective comments in the variable "message2" based on the input.
+  // when the user gives a number input and clicks on the submit button, the respective comments should be displayed.
+  // Here are the values and their respective comments
+  // Score of less than 0 or more than 100 — “This is not possible. An error has occurred.”
+  // Score of 0 to 19 — “That was a terrible score!”
+  // Score of 20 to 39 — “You know some things. Needs improvement.”
+  // Score of 40 to 69 — “You did a passable job, not bad!”
+  // Score of 70 to 89 — “That’s a great score. You really know your stuff.”
+  // Score of 90 to 100 — “What an amazing score!”
+
+  commentContainer2.innerHTML = message2;
+};
